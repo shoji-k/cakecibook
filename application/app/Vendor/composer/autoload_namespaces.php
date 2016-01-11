611dcf1874fcdc3435e37f4e2d996ce6a6ea8b6d
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-01-11 18:37:32 +0900

    install bdd plugins

--- application/app/Vendor/composer/autoload_namespaces.php
+++ application/app/Vendor/composer/autoload_namespaces.php
@@ -6,5 +6,19 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'PEAR' => array($vendorDir . '/pear/pear_exception'),
+    'Guzzle\\Stream' => array($vendorDir . '/guzzle/stream'),
+    'Guzzle\\Parser' => array($vendorDir . '/guzzle/parser'),
+    'Guzzle\\Http' => array($vendorDir . '/guzzle/http'),
+    'Guzzle\\Common' => array($vendorDir . '/guzzle/common'),
+    'Goutte' => array($vendorDir . '/fabpot/goutte'),
+    'DrSlump' => array($vendorDir . '/sizuhiko/spec-php/library'),
+    'Console' => array($vendorDir . '/pear/console_commandline'),
     'Composer\\Installers\\' => array($vendorDir . '/composer/installers/src'),
+    'Behat\\Mink\\Driver' => array($vendorDir . '/behat/mink-browserkit-driver/src', $vendorDir . '/behat/mink-goutte-driver/src'),
+    'Behat\\MinkExtension' => array($vendorDir . '/behat/mink-extension/src'),
+    'Behat\\Mink' => array($vendorDir . '/behat/mink/src'),
+    'Behat\\Gherkin' => array($vendorDir . '/behat/gherkin/src'),
+    'Behat\\CommonContexts' => array($vendorDir . '/behat/common-contexts'),
+    'Behat\\Behat' => array($vendorDir . '/behat/behat/src'),
 );
--- application/app/Vendor/composer/autoload_real.php
+++ application/app/Vendor/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInita5a4c765068a021c94e8dfab5ea82d56
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInita5a4c765068a021c94e8dfab5ea82d56', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        array_push($includePaths, get_include_path());
+        set_include_path(join(PATH_SEPARATOR, $includePaths));
+
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
@@ -40,6 +44,20 @@ class ComposerAutoloaderInita5a4c765068a021c94e8dfab5ea82d56
 
         $loader->register(true);
 
+        $includeFiles = require __DIR__ . '/autoload_files.php';
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequirea5a4c765068a021c94e8dfab5ea82d56($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequirea5a4c765068a021c94e8dfab5ea82d56($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
