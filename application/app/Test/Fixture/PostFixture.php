6fe5537f356d7fec5df191ebf2b74918b9560f7e
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-01-12 14:51:25 +0900

    regenerate post fixture

--- application/app/Test/Fixture/PostFixture.php
+++ application/app/Test/Fixture/PostFixture.php
@@ -6,21 +6,11 @@
 class PostFixture extends CakeTestFixture {
 
 /**
- * Fields
+ * Import
  *
  * @var array
  */
-	public $fields = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
-		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-		'body' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
-		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1)
-		),
-		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
-	);
+	public $import = array('model' => 'Post');
 
 /**
  * Records
@@ -32,8 +22,8 @@ class PostFixture extends CakeTestFixture {
 			'id' => 1,
 			'title' => 'Lorem ipsum dolor sit amet',
 			'body' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
-			'created' => '2016-01-11 14:12:30',
-			'modified' => '2016-01-11 14:12:30'
+			'created' => '2016-01-12 05:50:50',
+			'modified' => '2016-01-12 05:50:50'
 		),
 	);
 
